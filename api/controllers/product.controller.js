0c0701c6896314450ec3a67550f97850a064022c
Nhân Thành <nhan472569@gmail.com>
2021-10-07 13:00:06 +0700

    find product by id api

--- api/controllers/product.controller.js
+++ api/controllers/product.controller.js
@@ -4,3 +4,9 @@ module.exports.index = async function (req, res) {
   var products = await Product.find();
   res.json(products);
 };
+
+module.exports.getProductByID = async function (req, res) {
+  var productID = req.params.productID;
+  var product = await Product.findById(productID);
+  res.json(product);
+};
--- api/routes/product.route.js
+++ api/routes/product.route.js
@@ -5,5 +5,6 @@ var controller = require("../controllers/product.controller");
 var router = express.Router();
 
 router.get("/", controller.index);
+router.get("/:productID", controller.getProductByID);
 
 module.exports = router;
