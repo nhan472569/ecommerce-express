ce0f4d5bda3ae1389a471cf1d11d7aed6aaf5858
Nhân Thành <nhan472569@gmail.com>
2021-10-16 12:40:18 +0700

    get comment, post comment api

--- api/controllers/product.controller.js
+++ api/controllers/product.controller.js
@@ -1,5 +1,6 @@
 var Product = require("../../models/product.model");
 var Comment = require("../../models/comment.model");
+var User = require("../../models/user.model");
 
 module.exports.index = async function (req, res) {
   var products = await Product.find();
@@ -62,9 +63,28 @@ module.exports.getComment = async function (req, res) {
 };
 
 module.exports.postComment = async function (req, res) {
+  var userId = req.signedCookies.userId;
+  var content = req.body.comment;
   var productId = req.params.productID;
 
-  Comment.find({ productId: productId }, function (err, commentList) {
-    res.json(commentList);
+  if (userId) {
+    var user = await User.findById(userId);
+    var date = new Date();
+    var commentDate = date.toLocaleString(["vi-VN", "en-US"]);
+    await Comment.create({
+      user: user,
+      productId: productId,
+      content: content,
+      commentDate: commentDate,
+    });
+    res.json({
+      message: "Thêm bình luận thành công",
+      status: true,
+    });
+  }
+
+  res.json({
+    message: "Vui lòng đăng nhập",
+    status: false,
   });
 };
